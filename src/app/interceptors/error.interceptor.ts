bad4df8d53dfeab3480b488787bbccf6ce969f7a
karolh95 <karolh95@live.com>
2020-12-30 15:58:41 +0100

    Fixed: Error interceptor throws error with only one property instead of whole error object.

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { AuthenticationService } from '@services/authentication.service';
@@ -12,21 +12,20 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(
-	  private authenticationService: AuthenticationService
-  ) {}
+	constructor(
+		private authenticationService: AuthenticationService
+	) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-	return next.handle(request).pipe(catchError(this.errorHandler));
-  }
-
-  errorHandler = err => {
-	  
-	if (err.status == 401){
-		this.authenticationService.logout();
+	intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+		return next.handle(request).pipe(catchError(this.errorHandler));
 	}
 
-	const error = err.error ? err.error.message : err.status;
-	return throwError(error);
-  };
+	errorHandler = error => {
+
+		if (error.status == 401) {
+			this.authenticationService.logout();
+		}
+
+		return throwError(error);
+	};
 }
