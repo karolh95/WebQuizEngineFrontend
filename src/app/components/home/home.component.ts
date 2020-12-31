78aea300297effe2305ec15e75cd886419548471
karolh95 <karolh95@live.com>
2020-12-31 12:00:20 +0100

    Home page messages are generated using the Message array

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	messages: Message[];
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	ngOnInit(): void {
+
+		this.messages = [
+			{
+				title: 'Login & Registration',
+				date: new Date("2020-12-30"),
+				text: [
+					'Hi!',
+					'Login and Registration forms are now available!',
+					'Next station: Quiz Creation!'
+				]
+			},
+			{
+				title: 'Web Quiz is Offline',
+				date: new Date("2020-12-21"),
+				text: [
+					'Hello, there!',
+					'Web Quiz is under construction right now. It will be available soon!',
+					'Keep waiting!'
+				]
+			}
+		];
+	}
+
+}
+
+interface Message {
+	title: string;
+	date: Date,
+	text: string[];
 }
