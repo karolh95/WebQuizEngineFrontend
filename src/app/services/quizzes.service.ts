402f4c935d202ffb0440ae5e8483d8b2f710dd80
karolh95 <karolh95@live.com>
2021-01-01 14:24:55 +0100

    Introduced 'endpoint' variable  to simplifying http method calls

--- src/app/services/quizzes.service.ts
+++ src/app/services/quizzes.service.ts
@@ -8,37 +8,41 @@ import { Observable } from 'rxjs';
 })
 export class QuizzesService {
 
+	private endpoint: string;
+
 	constructor(
 		private http: HttpClient
-	) { }
+	) {
+		this.endpoint = `${environment.api}/quizzes`;
+	}
 
 	saveQuiz(quiz: Quiz): Observable<Quiz> {
-		return this.http.post<Quiz>(`${environment.api}/quizzes`, quiz);
+		return this.http.post<Quiz>(this.endpoint, quiz);
 	}
 
 	getQuizById(id: number): Observable<Quiz> {
-		return this.http.get<Quiz>(`${environment.api}/quizzes/${id}`);
+		return this.http.get<Quiz>(`${this.endpoint}/${id}`);
 	}
 
 	getQuizzes(request): Observable<Page<Quiz>> {
 		const params = request;
-		return this.http.get<Page<Quiz>>(`${environment.api}/quizzes`, { params });
+		return this.http.get<Page<Quiz>>(this.endpoint, { params });
 	}
 
 	getMyQuizzes(request): Observable<Page<Quiz>> {
 		const params = request;
-		return this.http.get<Page<Quiz>>(`${environment.api}/quizzes/my`, { params });
+		return this.http.get<Page<Quiz>>(`${this.endpoint}/my`, { params });
 	}
 
 	solveQuiz(quiz: Quiz): Observable<Answer> {
 		const id = quiz.id;
 		const body = { answer: quiz.answer };
-		return this.http.post<Answer>(`${environment.api}/quizzes/${id}/solve`, body);
+		return this.http.post<Answer>(`${this.endpoint}/${id}/solve`, body);
 	}
 
 	getCompletedQuizzes(request): Observable<Page<CompletedQuiz>> {
 		const params = request;
-		return this.http.get<Page<CompletedQuiz>>(`${environment.api}/quizzes/completed`, { params });
+		return this.http.get<Page<CompletedQuiz>>(`${this.endpoint}/completed`, { params });
 	}
 }
 
